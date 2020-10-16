ccb6835c4be653e519c5077afd98835b08ce34f9
xzb <1592352614@qq.com>
2020-10-16 08:51:21 +0800

    封装了发送请求函数

---
+++ api/goodsDetailApi.js
@@ -0,0 +1,15 @@
+import request from "./request.js";
+
+//根据商品id获取对应的商品详情
+export async function getGoodsDetailByGoodsId(goodsId){
+	let url = `getGoodsDetailByGoodsId?goodsId=${goodsId}`;
+	let res = await request(url);
+	return res.data.message[0];
+}
+
+//根据商品id获取对应的商品信息
+export async function getGoodsByGoodsId(goodsId){
+	let url = `getGoodsByGoodsId?goodsId=${goodsId}`;
+	let res = await request(url);
+	return res.data.message[0];
+}
