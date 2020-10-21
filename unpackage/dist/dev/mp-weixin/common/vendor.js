463971e742f847d5685546874870877493dc5a32
cj <1365584763@qq.com>
2020-10-21 08:41:56 +0800

    分类

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -904,7 +904,7 @@ function initData(vueOptions, context) {
     try {
       data = data.call(context); // 支持 Vue.prototype 上挂的数据
     } catch (e) {
-      if (Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.warn('根据 Vue 的 data 函数初始化小程序 data 失败，请尽量确保 data 函数中不访问 vm 对象，否则可能影响首次数据渲染速度。', data);
       }
     }
@@ -1936,9 +1936,9 @@ function normalizeComponent (
 /***/ }),
 
 /***/ 126:
-/*!*********************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/api/coupon.js ***!
-  \*********************************************************/
+/*!**********************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/api/coupon.js ***!
+  \**********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8268,7 +8268,7 @@ function type(obj) {
 
 function flushCallbacks$1(vm) {
     if (vm.__next_tick_callbacks && vm.__next_tick_callbacks.length) {
-        if (Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+        if (Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:flushCallbacks[' + vm.__next_tick_callbacks.length + ']');
@@ -8289,14 +8289,14 @@ function nextTick$1(vm, cb) {
     //1.nextTick 之前 已 setData 且 setData 还未回调完成
     //2.nextTick 之前存在 render watcher
     if (!vm.__next_tick_pending && !hasRenderWatcher(vm)) {
-        if(Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:nextVueTick');
         }
         return nextTick(cb, vm)
     }else{
-        if(Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance$1 = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance$1.is || mpInstance$1.route) + '][' + vm._uid +
                 ']:nextMPTick');
@@ -8381,7 +8381,7 @@ var patch = function(oldVnode, vnode) {
     });
     var diffData = this.$shouldDiffData === false ? data : diff(data, mpData);
     if (Object.keys(diffData).length) {
-      if (Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
           ']差量更新',
           JSON.stringify(diffData));
@@ -8789,9 +8789,9 @@ internalMixin(Vue);
 /***/ }),
 
 /***/ 20:
-/*!**********************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/api/homeApi.js ***!
-  \**********************************************************/
+/*!***********************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/api/homeApi.js ***!
+  \***********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8860,9 +8860,9 @@ function _getGoodsList() {_getGoodsList = _asyncToGenerator( /*#__PURE__*/_regen
 /***/ }),
 
 /***/ 21:
-/*!**********************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/api/request.js ***!
-  \**********************************************************/
+/*!***********************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/api/request.js ***!
+  \***********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8923,9 +8923,9 @@ module.exports = g;
 /***/ }),
 
 /***/ 38:
-/*!**********************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/util/isarzt.js ***!
-  \**********************************************************/
+/*!***********************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/util/isarzt.js ***!
+  \***********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8951,9 +8951,9 @@ isUserInfo() {return _isUserInfo.apply(this, arguments);}function _isUserInfo() 
 /***/ }),
 
 /***/ 4:
-/*!******************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/pages.json ***!
-  \******************************************************/
+/*!*******************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/pages.json ***!
+  \*******************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -8962,9 +8962,9 @@ isUserInfo() {return _isUserInfo.apply(this, arguments);}function _isUserInfo() 
 /***/ }),
 
 /***/ 47:
-/*!*************************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/api/shopingCar.js ***!
-  \*************************************************************/
+/*!**************************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/api/shopingCar.js ***!
+  \**************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8980,9 +8980,9 @@ function getShopingCarByUser(_x) {return _getShopingCarByUser.apply(this, argume
 /***/ }),
 
 /***/ 56:
-/*!*********************************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/api/classifiedGoodsApi.js ***!
-  \*********************************************************************/
+/*!**********************************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/api/classifiedGoodsApi.js ***!
+  \**********************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9006,9 +9006,9 @@ function _getClassifiedGoods() {_getClassifiedGoods = _asyncToGenerator( /*#__PU
 /***/ }),
 
 /***/ 73:
-/*!*****************************************************************!*\
-  !*** D:/2020-毕业项目/temp/angel-tz-projects/api/goodsDetailApi.js ***!
-  \*****************************************************************/
+/*!******************************************************************************!*\
+  !*** C:/Users/Administrator/Desktop/angel-tz-projects/api/goodsDetailApi.js ***!
+  \******************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
